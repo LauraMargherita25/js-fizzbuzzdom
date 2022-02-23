e65b8ba41ba2ceb30f48b306be6bb75ed9f0fc87
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-23 17:40:35 +0100

    Add append

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 let itemContainer = document.querySelector(".item_container");
 
 for (let i = 1; i <= 100; i++) {
+
+    const square = document.createElement("div");
+    square.classList.add("box");
+
     if(i % 3 == 0 && i % 5 == 0){
-        itemContainer.innerHTML += `<div class="box blue">fizzbuzz</div>`
+        // itemContainer.innerHTML += `<div class="box blue">fizzbuzz</div>`;
+        square.classList.add("blue")
+        square.innerHTML = ("fizzbuzz");
     }else if (i % 5 == 0){
-        itemContainer.innerHTML += `<div class="box green">buzz</div>`
+        // itemContainer.innerHTML += `<div class="box green">buzz</div>`;
+        square.classList.add("green")
+        square.innerHTML = ("buzz");
     }else if(i % 3 == 0){
-        itemContainer.innerHTML += `<div class="box red">fizz</div>`
+        // itemContainer.innerHTML += `<div class="box red">fizz</div>`;
+        square.classList.add("red")
+        square.innerHTML = ("fiz");
     }else{
-        itemContainer.innerHTML += `<div class="box">${i}</div>`
+        // itemContainer.innerHTML += `<div class="box">${i}</div>`;
+        square.classList.add("box")
+        square.innerHTML = i;
     }
+
+    itemContainer.append(square);
 }
