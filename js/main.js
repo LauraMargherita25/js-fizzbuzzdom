0bef364c95cb9da8433888e42d21faf91c07a702
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-23 16:46:41 +0100

    loop

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 let itemContainer = document.querySelector(".item_container");
 
-for (let i = 0; i <= 100; i++) {
-    itemContainer.innerHTML += `<div class="box">${i}</div>`
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 == 0){
+        itemContainer.innerHTML += `<div class="box red">fizz</div>`
+    }else if (i % 5 == 0){
+        itemContainer.innerHTML += `<div class="box green">buzz</div>`
+    }else if(i % 3 && i % 5){
+        itemContainer.innerHTML += `<div class="box blue">fizzbuzz</div>`
+    }else{
+        itemContainer.innerHTML += `<div class="box">${i}</div>`
+    }
 }
