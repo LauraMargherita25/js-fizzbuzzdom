5a2daa97b9b3c344c4b7763289c3f5cab6f62509
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-02-23 17:19:19 +0100

    Now the loop is working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let itemContainer = document.querySelector(".item_container");
 
 for (let i = 1; i <= 100; i++) {
-    if(i % 3 == 0){
-        itemContainer.innerHTML += `<div class="box red">fizz</div>`
+    if(i % 3 == 0 && i % 5 == 0){
+        itemContainer.innerHTML += `<div class="box blue">fizzbuzz</div>`
     }else if (i % 5 == 0){
         itemContainer.innerHTML += `<div class="box green">buzz</div>`
-    }else if(i % 3 && i % 5){
-        itemContainer.innerHTML += `<div class="box blue">fizzbuzz</div>`
+    }else if(i % 3 == 0){
+        itemContainer.innerHTML += `<div class="box red">fizz</div>`
     }else{
         itemContainer.innerHTML += `<div class="box">${i}</div>`
     }
